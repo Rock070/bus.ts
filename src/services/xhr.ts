876cb23e2aea7df813cc49a997cc5bf089358cb4
Rock Wang <f2021021818@W06F09.local>
2022-01-18 17:45:58 +0800

    調整寫法

--- src/services/xhr.ts
+++ src/services/xhr.ts
@@ -2,32 +2,35 @@ import axios from 'axios'
 import { getAuthorizationHeader } from './helper'
 
 interface Ioptions {
-  method: string
+  method: Method
   url: string
   data?: string | null
   params?: string | null
 }
 
-const xhr = async (options: Ioptions) => {
-  const {
-    method = 'GET',
-    url = '/',
-    data = null,
-    params = null,
-  } = { ...options }
-
+const xhr = async ({
+  method = 'GET',
+  url = '/',
+  data = null,
+  params = null,
+} :Ioptions) => {
   const BASE_URL = 'https://ptx.transportdata.tw/MOTC/'
-  const targetUrl = BASE_URL + options.url
+  const targetUrl = BASE_URL + url
+  // const arr: Array<number | string | boolean> = [1, 2, 3, 4, '2', true]
+  function sum(arr:Array<number> = [123]) {
+    // let args: number[] = arguments;
+    console.log(arguments)
+}
+sum([12391230])
 
   return await axios({
     method,
     url: targetUrl,
-    data, 
+    data,
     params,
     // headers: getAuthorizationHeader(),
   })
     .then(res => res.data)
     .catch(err => Promise.reject(err))
 }
-
 export default xhr
